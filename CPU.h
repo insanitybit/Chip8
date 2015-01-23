fd5171471c1529557a1f53adfde882fef760ea30
insanitybit <insanitybit@gmail.com>
2015-01-23 13:49:16 -0500

    Set up annoying switch

--- CPU.h
+++ CPU.h
@@ -46,23 +46,56 @@ private:
 	WORD psize; // size of binary + pc
 	std::mt19937 gen{std::random_device{}()};
 
+
+	void OP_0NNN();
+	void OP_00E0();
+	void OP_00EE();
+	void OP_1NNN();
+	void OP_2NNN();
+	void OP_3XNN();
+	void OP_4XNN();
+	void OP_5XY0();
+	void OP_6XNN();
+	void OP_7XNN();
+	void OP_8XY0();
+	void OP_8XY1();
+	void OP_8XY2();
+	void OP_8XY3();
+	void OP_8XY4();
+	void OP_8XY5();
+	void OP_8XY6();
+	void OP_8XY7();
+	void OP_8XYE();
+	void OP_9XY0();
 	void OP_ANNN();
 	void OP_BNNN();
 	void OP_CXNN();
 	void OP_DXYN();
 	void OP_EX9E();
 	void OP_EXA1();
+	void OP_FX07();
+	void OP_FX0A();
+	void OP_FX15();
+	void OP_FX18();
+	void OP_FX1E();
+	void OP_FX29();
+	void OP_FX33();
+	void OP_FX55();
+	void OP_FX65();
+
+
+
 };
 
 CPU::CPU(){
 	pc = 0x200; // "most programs [...] begin at memory location 0x200"
+
 }
 
 // Create string out of gamefile, store that string 512 bytes into memory
 
 void CPU::load(const std::string& s){
 	std::ifstream file(s);
-
 	assert(file);
 
 	file.seekg( 0, std::ios::end );
@@ -112,8 +145,87 @@ bool CPU::decode(){
 	BYTE second = ((op >> 8) & 0x000F);
 	BYTE third 	= ((op >> 4) & 0x000F);
 	BYTE fourth = (op & 0x000F);
-
+	
 	switch(first){
+		case 0x0:
+				switch(fourth)
+				{
+					case 0x0:
+							OP_00EE();
+							break;
+					case 0xE:
+							OP_00E0();
+							break;
+					default:
+							
+							OP_0NNN();
+				}
+				break;
+
+		case 0x1:
+				OP_1NNN();
+				break;
+
+		case 0x2:
+				OP_2NNN();
+				break;
+
+		case 0x3:
+				OP_3XNN();
+				break;
+
+		case 0x4:
+				OP_4XNN();
+				break;
+
+		case 0x5:
+				OP_5XY0();
+				break;
+
+		case 0x6:
+				OP_6XNN();
+				break;
+
+		case 0x7:
+				OP_7XNN();
+				break;
+
+		case 0x8:
+				switch(fourth)
+				{
+					case 0x0:
+							OP_8XY0();
+							break;
+					case 0x1:
+							OP_8XY1();
+							break;
+					case 0x2:
+							OP_8XY2();
+							break;
+					case 0x3:
+							OP_8XY3();
+							break;
+					case 0x4:
+							OP_8XY4();
+							break;
+					case 0x5:
+							OP_8XY5();
+							break;
+					case 0x6:
+							OP_8XY6();
+							break;
+					case 0x7:
+							OP_8XY7();
+							break;
+					case 0xE:
+							OP_8XYE();
+							break;
+				}
+				break;
+
+		case 0x9:
+				OP_9XY0();
+				break;
 
 		case 0xA:
 				OP_ANNN();
@@ -132,16 +244,69 @@ bool CPU::decode(){
 				break;
 
 		case 0xE:
-				switch(fourth){
+				switch(fourth)
+				{
 					case 0xE:
 							OP_EX9E();
 							break;
+
 					case 0x1:
 							OP_EXA1();
 							break;
 				}
 				
 				break;
+
+		case 0xF:
+				switch(third)
+				{
+					case 0x0:
+						switch(fourth)
+						{
+							case 0x7:
+									OP_FX07();
+									break;
+
+							case 0xA:
+									OP_FX0A();
+									break;
+						}
+						break;
+					case 0x1:
+						switch(fourth)
+						{
+							case 0x5:
+									OP_FX15();
+									break;
+
+							case 0x8:
+									OP_FX18();
+									break;
+
+							case 0xE:
+									OP_FX1E();
+									break;
+
+						}
+					case 0x2:
+							OP_FX29();
+							break;
+
+					case 0x3:
+							OP_FX33();
+							break;
+
+					case 0x5:
+							OP_FX55();
+							break;
+
+					case 0x6:
+							OP_FX65();
+							break;
+
+				}
+
+			break;
 	}
 
 	return true;
@@ -197,6 +362,141 @@ void CPU::OP_EX9E(){
 void CPU::OP_EXA1(){
 
 }
+
+// Calls RCA 1802 program at address NNN.
+void CPU::OP_0NNN(){
+
+}
+
+// Clears the screen.
+void CPU::OP_00E0(){
+
+}
+
+// Returns from a subroutine.
+void CPU::OP_00EE(){
+
+}
+
+// Jumps to address NNN.
+void CPU::OP_1NNN(){
+	// pc = (opcode & 0x0FFF);
+}
+
+void CPU::OP_2NNN(){
+
+}
+
+// Skips the next instruction if VX equals NN.
+void CPU::OP_3XNN(){
+	BYTE NN = (opcode & 0x00FF);
+	if(NN == V[((opcode >> 8) & 0x000F)])
+		pc += 2;
+}
+
+// 4XNN	Skips the next instruction if VX doesn't equal NN.
+
+void CPU::OP_4XNN(){
+	BYTE NN = (opcode & 0x00FF);
+	if(NN != V[((opcode >> 8) & 0x000F)])
+		pc += 2;
+}
+
+// 5XY0	Skips the next instruction if VX equals VY.
+void CPU::OP_5XY0(){
+	BYTE X = ((opcode >> 8) & 0x000F);
+	BYTE Y = ((opcode >> 4) & 0x000F);
+
+	if(X == Y)
+		pc += 2;
+}
+
+// 6XNN	Sets VX to NN.
+void CPU::OP_6XNN(){
+
+}
+
+// 7XNN	Adds NN to VX.
+void CPU::OP_7XNN(){
+
+}
+
+void CPU::OP_8XY0(){
+
+}
+
+void CPU::OP_8XY1(){
+
+}
+
+void CPU::OP_8XY2(){
+
+}
+
+void CPU::OP_8XY3(){
+
+}
+
+void CPU::OP_8XY4(){
+
+}
+
+void CPU::OP_8XY5(){
+
+}
+
+void CPU::OP_8XY6(){
+
+}
+
+void CPU::OP_8XY7(){
+
+}
+
+void CPU::OP_8XYE(){
+
+}
+
+void CPU::OP_9XY0(){
+
+}
+
+void CPU::OP_FX07(){
+
+}
+
+void CPU::OP_FX0A(){
+
+}
+
+void CPU::OP_FX15(){
+
+}
+
+void CPU::OP_FX18(){
+
+}
+
+void CPU::OP_FX1E(){
+
+}
+
+void CPU::OP_FX29(){
+
+}
+
+void CPU::OP_FX33(){
+
+}
+
+void CPU::OP_FX55(){
+
+}
+
+void CPU::OP_FX65(){
+
+}
+
 
 
 #endif
